3bcb79ded2478c48ff7dd1b8f9b0899818c6548d
getrojob <getrojob@gmail.com>
2021-08-29 18:50:16 -0300

    Comment description ContextFactory

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -7,7 +7,7 @@ namespace Api.Data.Context
     {
         public MyContext CreateDbContext(string[] args)
         {
-            // Usado para criar as Migrações
+            // Usado para criar as Migrações em tempo de designer da aplicação
             var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=vetrigo";
             // var connectionString = "Server=.\\SQLEXPRESS;Initial Catalog=dbAPI_2;Trusted_Connection=True;MultipleActiveResultSets=true";
 
