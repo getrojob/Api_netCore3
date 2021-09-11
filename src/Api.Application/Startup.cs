01eb5ef4ebf3d01e091ffd6aca11e4d006761dcd
getrojob <getrojob@gmail.com>
2021-09-11 14:54:31 -0300

    Add Swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -32,6 +33,27 @@ namespace application
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API com AspNetCore 3.1",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("https://www.linkedin.com/in/getuliorodriguez/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Getulio Silva",
+                        Email = "getrojob@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/getuliorodriguez/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://www.linkedin.com/in/getuliorodriguez/")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +64,12 @@ namespace application
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
 
             app.UseAuthorization();
