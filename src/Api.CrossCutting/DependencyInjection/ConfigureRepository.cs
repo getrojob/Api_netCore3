08be0d0498ebcafe6908df2de92fbf0282b428c1
getrojob <getrojob@gmail.com>
2021-09-12 23:11:33 -0300

    feat: Dependency ConfigureRepository

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,6 +1,8 @@
 using Api.Data.Context;
+using Api.Data.Implementations;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Api.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,8 +13,16 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
+
+
             serviceCollection.AddDbContext<MyContext>(
                 options => options.UseMySql("Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=vetrigo")
+
+
+            // serviceCollection.AddDbContext<MyContext>(options =>
+            // options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); 
+            // options.UseSqlServer("Server=.\\SQLEXPRESS;Initial Catalog=dbAPI;Trusted_Connection=True;MultipleActiveResultSets=true")
             );
         }
     }
