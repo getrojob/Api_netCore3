bea39f1cb80e03a03306932b30e54b533f3c7164
getrojob <getrojob@gmail.com>
2021-10-02 17:39:46 -0300

    fix: remove reference sqlite

--- src/Api.Data.Test/BaseTest.cs
+++ src/Api.Data.Test/BaseTest.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using Api.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,23 +14,14 @@ namespace Api.Data.Test
     }
     public class DbTeste : IDisposable
     {
-        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}.db";
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
         public ServiceProvider ServiceProvider { get; private set; }
-        public string DbPath { get; private set; }
-
 
         public DbTeste()
         {
-            // var folder = Environment.SpecialFolder.LocalApplicationData;
-            // var path = Environment.GetFolderPath(folder);
-            var path = "src\\Api.Data\\baseTest";
-            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}" + dataBaseName;
-
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddDbContext<MyContext>(o =>
-            // o.UseMySql($"Server=localhost;Port=3306;Database={dataBaseName};Uid=root;Pwd=vetrigo"),
-            // o.UseMySql($"Server=localhost;Port=3306;Database={dataBaseName};Uid=root;Pwd=vetrigo"),
-            o.UseSqlite($"Data Source={DbPath}"),
+            o.UseMySql($"Server=localhost;Port=3306;Database={dataBaseName};Uid=root;Pwd=vetrigo"),
             ServiceLifetime.Transient);
 
             ServiceProvider = serviceCollection.BuildServiceProvider();
