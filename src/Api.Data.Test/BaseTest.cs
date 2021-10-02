f57435272350ecbf31b324828f407508b21e2fd7
getrojob <getrojob@gmail.com>
2021-10-02 17:30:36 -0300

    feat: add reference sqlite

--- src/Api.Data.Test/BaseTest.cs
+++ src/Api.Data.Test/BaseTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Api.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,14 +15,23 @@ namespace Api.Data.Test
     }
     public class DbTeste : IDisposable
     {
-        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}.db";
         public ServiceProvider ServiceProvider { get; private set; }
+        public string DbPath { get; private set; }
+
 
         public DbTeste()
         {
+            // var folder = Environment.SpecialFolder.LocalApplicationData;
+            // var path = Environment.GetFolderPath(folder);
+            var path = "src\\Api.Data\\baseTest";
+            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}" + dataBaseName;
+
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddDbContext<MyContext>(o =>
-            o.UseMySql($"Server=localhost;Port=3306;Database={dataBaseName};Uid=root;Pwd=vetrigo"),
+            // o.UseMySql($"Server=localhost;Port=3306;Database={dataBaseName};Uid=root;Pwd=vetrigo"),
+            // o.UseMySql($"Server=localhost;Port=3306;Database={dataBaseName};Uid=root;Pwd=vetrigo"),
+            o.UseSqlite($"Data Source={DbPath}"),
             ServiceLifetime.Transient);
 
             ServiceProvider = serviceCollection.BuildServiceProvider();
