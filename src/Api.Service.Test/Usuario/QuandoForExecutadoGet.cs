abf82536558d370c63c170b146cd97a19baa498b
getrojob <getrojob@gmail.com>
2021-10-09 21:52:24 -0300

    feat: add mock test in method get

---
+++ src/Api.Service.Test/Usuario/QuandoForExecutadoGet.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using Xunit;
+
+namespace Api.Service.Test.Usuario
+{
+    public abstract class QuandoForExecutadoGet : UsuarioTestes
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É Possivel Executar o Método GET.")]
+        public async Task E_Possivel_Executar_Metodo_Get()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Get(IdUsuario)).ReturnsAsync(userDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Get(IdUsuario);
+            Assert.NotNull(result);
+            Assert.True(result.Id == IdUsuario);
+            Assert.Equal(NomeUsuario, result.Name);
+        }
+    }
+}
